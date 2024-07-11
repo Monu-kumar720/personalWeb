3cddda24c6332ac4abb3ac1f8fab70993cd1e8a5
Monu Kumar <75105879+Monu-kumar720@users.noreply.github.com>
2024-07-11 21:59:46 +0530

    initial commit

---
+++ a.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+class a
+{
+    public:
+    string name;
+    int age;
+};
+
+
+
+int main()
+{
+    a s;
+    s.name="monu";
+    s.age=21;
+    cout<<s.age<<endl;
+}
